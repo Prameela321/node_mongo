8f2653ecb738b8e9beeffcf006cddd6150da9a5a
prameela <thandu.prameela321@gmail.com>
2025-01-06 10:26:08 +0530

    remove middleware to list restaurant

--- routes/restaturantRoute.js
+++ routes/restaturantRoute.js
@@ -2,7 +2,7 @@ const restaurantController = require('../controllers/restaurantController');
 // const {verifyToken} = require('../middleware/verifyToken');
 function restaurantRoutes(app){
     app.post('/createRestaurant',restaurantController.createRestaurant);
-    app.get('/listRestaurant',verifyToken,restaurantController.listRestaurant);
+    app.get('/listRestaurant',restaurantController.listRestaurant);
     app.get('/filterRestaurant/:id',restaurantController.filterRestaurant);
     app.put('/updateRestaurant/:id',restaurantController.updateRestaurantById);
     app.delete('/deleteRestaurant/:id',restaurantController.deleteRestaurantById);
